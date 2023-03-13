77baf1990fee8de990554cf8b982647cae42a415
Zonwiezhu <102626057+Zonwiezhu@users.noreply.github.com>
2023-03-13 19:14:40 +0100

    Verkeerde Png
    Moest JPG zijn waardoor er een error is.

--- src/views/Lichamelijke_Klachten.vue
+++ src/views/Lichamelijke_Klachten.vue
@@ -85,7 +85,7 @@
 
                 <div class= "main-banner1">
                     <div class="img-container">
-                        <img src="/src/assets/Afbeeldingen/Massage1.JPG" alt="" >
+                        <img src="/src/assets/Afbeeldingen/Massage1.png" alt="" >
                     </div>
                 </div>
                     
