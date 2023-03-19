e0f502fdc0c9098b9a928cb962bf812c0440804b
Zonwiezhu <102626057+Zonwiezhu@users.noreply.github.com>
2023-03-19 17:51:04 +0100

    Lichamelijke klachten update
    Update lichamelijke klachten qua text.

--- src/views/Lichamelijke_Klachten.vue
+++ src/views/Lichamelijke_Klachten.vue
@@ -12,71 +12,142 @@
                 <section>
                     <details>
                         <summary>Slaapstoornissen</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.</p>
+                        <p>
+                            Slaapstoornissen ijn problemen die de kwaliteit en/of de duur van de slaap verstoren.
+                            Er zijn verschillende soorten slaapstoornissen, waaronder insomnie, slaapapneu, en narcolepsie.
+                            Deze slaapstoornissen kunnen leiden tot vermoeidheid, prikkelbaarheid en concentratieproblemen overdag.
+                            De oorzaken van slaapstoornissen kunnen verschillen, waaronder stress, angst, depressie, medicatiegebruik, lichamelije aandoeningen en slaapomgeving.
+                        </p>
                     </details>
                     <details>
                         <summary>Rusteloosheid</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex.</p>
+                        <p>
+                            Rusteloosheid is een gevoel van innerlijke onrust, nervositeit of prikkelbaarheid. 
+                            Het kan leiden tot een verhoogde hartslag, zweten en moeite hebben om stil te zitten of te ontspannen.
+                            Rusteloosheid kan worden veroorzaakt door stress, angst, medicatiegebruik of een onderliggende medische aandoening.
+                            Het kan invloed hebben op de kwaliteit can leven, omdat het intergerentie kan veroorzaken bij dagelijkse activiteiten en slaap.
+                        </p>
                     </details>
                     <details>
                         <summary>Angst</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Angst is een gevoel van onrust, zorgen of onbehagen, vaak gepaard met lichamelijke sensaties zoals een verhoogde hartslag, zweten en een droge mond.
+                            Het is een normale menselijke reactie op stressvolle situatie, maar kan ook een aandoening zijn wanneer het zich voordoet zonder aanwijsbare oorzaak en het dagelijkse leven belemmert.
+                        </p>
                     </details>
                     <details>
                         <summary>Depressiviteit</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Depressiviteit is een stemmingsstoornis die wordt gekenmerkt door gevoelens van verdriet, hopeloosheid, vermoeidheid en een verlies van intresse in activiteiten die eerder als plezietig werden ervaren.
+                            Het kan ook gepaard gaan met slaapproblemen, eetproblemen en verminderde concentratie en besluitvorming. 
+                            Depressiviteit kan verschillende oorzaken hebben, zoals biologische factoren, levensgebeurtenissen, genetica en medicatie.
+                        </p>
                     </details>
                     <details>
                         <summary>Allergie</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Een allergie is een overreactie van het immuunsysteem op een onschadelijke stof, zoals pollen, huisstofmijn of voedsel. 
+                            Symptonen van allergie kunnen verschillen van mild tot ernstig.
+                            Allergie kunnen erfelijk zijn en kunnen op elk moment in het leven ontstaan.
+                        </p>
                     </details>
                     <details>
                         <summary>Hooikoorts</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Hooikoorts is een allergische reactie op stuifmeel van grassen, bomen en planten. 
+                            Synptonen van hooikoorts kunnen zijn: niezen, jeukende ogen, neusverstopping en een loopneus.
+                            Deze symptonen kunnen de dagelijkse activiteiten en de slaap beinvloeden. 
+                            Hooitkoorts komt meestal voor in de lente en de zomer wanneer de pollenconcentraties hoog zijn.
+                        </p>
                     </details>
                     <details>
                         <summary>Hernia</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Een hernia is een aandoening waarbij een deel van een orgaan of weefsel door een opening in de omringende spieren of weefsel wordt geduwd.
+                            Het komt het meest voor in de rug, waarbij een deel van de tussenwervelschijf uitpuilt en tegen een zenuw drukt, wat kan leiden tot pijn, zwate en gevoelloosheid in de benen.
+                            Risicofactoren voor een hernia zijn onder ander zwaar tillen, overgewicht, zittend werk en veroudering.
+                        </p>
                     </details>
                     <details>
                         <summary>Astma; Bronchitis</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Astma is een chronologische ontsteking van de luchtwegen die leidt tot vernauwing en obstructie van de luchtwegen, waardoor ademhalingsproblemen ontstaan.
+                            Symptonen van astma zijn onder andere piepende ademhaling, kortademigheid, hoesten en beklemming op de borst.
+                            Astma kan worden veroorzaakt door allergenen, luchtvervuiling, roken, infecties en lichamelijke activiteit.
+                        </p>
                     </details>
                     <details>
                         <summary>Trigeminusneuralgie</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Trigeminusneuralgie is een aandoening waarbij hevige, scherpe pijn optreedt in de gezichtszenuw. 
+                            De pijn kan worden veroorzaakt door verschillende triggers, zoals het aanraken van het gezicht, spreken of kauwen.
+                            Trigeminusneuralgie kan worden veroorzaakt door compressie van de zenuw of een aandoening zoals multiple scelrose.
+                        </p>
                     </details>
                     <details>
                         <summary>Nek en rug klachten</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Nek- en rugklachten zijn veelvoorkomende aandoeningen die kunnen worden veroorzaakt door verschillende factoren, zoals een slechte houding, stress, letsel of een onderliggende medische aandoening.
+                            Symptonen kunnen variëren van pijn en stijfheid tot gevoelloosheid en tintelingen. Deze klachten kunnen het uit
+                        </p>
                     </details>
                     <details>
                         <summary>Eczeem</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Eczeem is een huidaandoening die wordt gekenmerkt door een ontstoken en jeukende huid. 
+                            Het kan worden veroorzaakt door een overactief immuunsysteem, genetische aanleg en externe factoren zoals allergenen, irriterende stoffen en stress.
+                            Symptonen van eczeem zijn onder andere roodheid, zwelling, schilfering en korstvormig van de huid.
+                        </p>
                     </details>
                     <details>
                         <summary>Stijfheid</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                          Stijfheid is een gevoel van stramheid of moeite om bewegingen uit te voeren, vooral na langdurig stilzitten of inactiviteit. 
+                          Het kan worden veroorzaakt door verschillende factoren, zoals veroudering, letsel, ziekte of een gebrek aan lichaamsbeweging.
+                          Symptonen van stijfheid zijn onder andere moeite met bewegen, pijn en spierspanning.
+                        </p>
                     </details>
                     <details>
                         <summary>Vruchtbaarheid</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Vruchtbaarheid verwijst naar het vermogen van een persoon om een zwangerschap tot stant te brengen en te dragen. 
+                            Bij vrouwen kan vruchtbaarheid worden beïnvloed door verschillende factoren, zoals leeftijd, hormonale disbalans, 
+                            onderliggende medische aandoeningen of leefstijlfactoren.
+                        </p>
                     </details>
                     <details>
                         <summary>Acne</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Acne is een huidaandoening die wordt gekenmerkt door onstoken en verstopte talgklieren.
+                            Het wordt vaak geassocieerd met de puberteit, maar kan op elke leeftijd voorkomen. 
+                            Acne kan worden veroorzaakt door homonale vernaderingen, bacteriën, overmatige talgproductie en verstopte poriën.
+                            Symptonen can acne zijn onder andere puistjes, mee-eters, rode bultjes en soms littekens.
+                        </p>
                     </details>
                     <details>
                         <summary>Jeuk</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Jeuk is een onaangenaam gevoel op de huid, wardoor men wil krabben om verlichting te krijgen.
+                            Het kan worden veroorzaakt door verschillende factoren, zoals allergieën, insectenbeten, droge huid, huidaandoeningen, infecties, medicijnen en ziektes.
+                            Symptonen van jeuk zijn onder andere roodheid, bultjes en huiduitslag.
+                        </p>
                     </details>
                     <details>
                         <summary>Schildklieraandoening</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Een schildklieraandoening treedt op wanneer de schildklier, een klein orgaan in de hals dat hormomen produceert die het metabolisme reguleren, niet goed functioneert.
+                            Er zijn twee hoofdtypen van schildklieraandoeningen:  hypothyreoïdie (te lage schildklierhormoonproductie) en hyperthyreoïdie (te veel schildklierhormoonproductie).
+                            Symptonen van een schildklieraandoening variëren afhankelijk van het type en de ernst van de aandoening, maar kunnen onder andere vermoeidheid, gewichtsveranderingen, haaruitval, droge huid, gevoeligheid voor kou of warmte en stemmingswisselingen omvatten.
+                        </p>
                     </details>
                     <details>
                         <summary>Migraine</summary>
-                        <p>Lorem ipsum dolor sit amet, eu alia suscipit mei. Reque iriure delectus vix id, ex sed forensibus suscipiantur. In eos exerci mollis apeirian, an qui latine alienum. Ad mea libris maluisset, consul assueverit sea ex. </p>
+                        <p>
+                            Migraine is een neurologische aandoening die wordt gekenmerkt door terugkerende hoofdpijn die kan worden vegezeld door andere symptonen zoals misselijkheid, braken en gevoeligheid voor licht en geluid.
+                            Migraineaanvallen kunnen enkele uren tot meerdere dagen duren en kunnen verstorend zijn voor de dagelijkse activiteiten. 
+                            De oorzaken van migraine zijn nog niet volledig begrepen, maar het wordt verondersteld dat genetische factoren en omgevingsfactoren een rol spelen.
+
+                        </p>
                     </details>
                 </section>
             </div>
@@ -85,13 +156,13 @@
 
                 <div class= "main-banner1">
                     <div class="img-container">
-                        <img src="/src/assets/Afbeeldingen/Massage1.png" alt="" >
+                        <img src="/src/assets/Afbeeldingen/Kruiden.png" alt="" >
                     </div>
                 </div>
                     
                 <div class= "main-banner2">
                     <div class="img-container">
-                        <img src="/src/assets/Afbeeldingen/Massage1.png" alt="" >
+                        <img src="/src/assets/Afbeeldingen/HmeiBinnen2.png" alt="" >
                     </div>
                 </div>
             </div>
@@ -203,11 +274,16 @@ section {
     }
 
 
-@media(max-width: 975p) {
+@media(max-width: 975px) {
     .banner-content {
         flex-wrap: wrap;
+        display: inline;
      }
-
+     .img-container img{
+      width: 100%;
+      object-fit: cover;
+      padding-top: 10px;
+    }
     .banner-content > div {
           flex: 0 0 100%;
           max-width: 100%;
